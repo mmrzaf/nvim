c3b930ce367a12aac8dafe96acd3df182f4f200f
m.farkhondeh <pheri@paya.bigdata.net>
2025-10-08 16:00:32 +0330

    feat(term): toggleterm

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    cmd = { 'ToggleTerm', 'TermExec' },
+    keys = {
+      { '<leader>tt', '<cmd>ToggleTerm direction=float<cr>', desc = 'Toggle terminal' },
+    },
+    opts = {
+      size = 12,
+      open_mapping = [[<Nop>]],
+      shade_terminals = false,
+      direction = 'float',
+      float_opts = { border = 'rounded' },
+    },
+  },
+}
