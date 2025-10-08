b172db440e4c1eb0ee004a6cc10d46284a8f3732
m.farkhondeh <pheri@paya.bigdata.net>
2025-10-08 16:00:18 +0330

    feat(ui): themes (kanagawa, catppuccin)

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,44 @@
+return {
+	{
+		"rebelot/kanagawa.nvim",
+		name = "kanagawa",
+		priority = 1000,
+		opts = {
+			compile = true,
+			theme = "dragon",
+			overrides = function(colors)
+				return {
+					Normal = { bg = colors.theme.ui.bg_dim },
+				}
+			end,
+		},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = true,
+		priority = 900,
+		opts = {
+			flavour = "mocha",
+			transparent_background = false,
+			styles = {
+				comments     = { "italic" },
+				conditionals = {}, -- "none"
+				loops        = {}, -- "none"
+				functions    = { "bold" },
+				keywords     = {}, -- "none"
+				strings      = {}, -- "none"
+				variables    = {}, -- "none"
+			},
+			integrations = {
+				fzf = true,
+				lualine = true,
+				mason = true,
+				cmp = true,
+				treesitter = true,
+				gitsigns = true,
+			},
+		},
+	}
+
+}
