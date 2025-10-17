7371fb1c5f4fc6db2397276a1003dbf686a7e769
Mohammadreza-Farkhondeh <mohammr.farkhonde12@shairf.edu>
2025-10-17 10:08:13 +0330

    turned off autoformat and add keybinding on demand instead

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,17 +1,29 @@
 return {
 	{
 		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
+		event = { "VeryLazy" },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				desc = "Format file (on demand)",
+				mode = "n",
+			},
+		},
 		opts = {
 			notify_on_error = false,
-			format_on_save = function(bufnr)
-				local max = 300 * 1024
-				local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(bufnr))
-				if ok and stats and stats.size > max then
-					return nil
-				end
-				return { lsp_fallback = true, timeout_ms = 2000 }
-			end,
+			format_on_save = false,
+			--format_on_save = function(bufnr)
+			--	local max = 300 * 1024
+			--	local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+			--	if ok and stats and stats.size > max then
+			--		return nil
+			--	end
+			--	return { lsp_fallback = true, timeout_ms = 2000 }
+			--end,
+
 			formatters_by_ft = {
 				lua = { "stylua" },
 				go = { "gofumpt", "goimports", stop_after_first = true },
