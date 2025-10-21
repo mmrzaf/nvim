1c2f34bb39f14d047e38954f868e0cba8596a88a
m.farkhondeh <pheri@paya.bigdata.net>
2025-10-21 13:35:22 +0330

    feat: added lazygit gg to plugins

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -14,4 +14,21 @@ return {
 			current_line_blame = false,
 		},
 	},
+	{
+		"kdheepak/lazygit.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		cmd = "LazyGit",
+		keys = {
+			{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
+		},
+		config = function()
+			-- floating window settings (optional tweaks)
+			vim.g.lazygit_floating_window_use_plenary = 1
+			vim.g.lazygit_floating_window_winblend = 0
+			vim.g.lazygit_floating_window_scaling_factor = 0.9
+			vim.g.lazygit_use_neovim_remote = 1
+		end,
+	},
 }
